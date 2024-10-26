e87c01280993a8c302246c6f6851107882814dd3
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 02:32:39 +0300

    Добавьте файлы проекта.

---
+++ Leap/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Leap
+{
+    public static bool IsLeapYear(int year)
+    {
+        if (year % 400 == 0)
+            return true;
+        if (year % 100 == 0)
+            return false;
+        return year % 4 == 0;
+    }
+}
